07b53d721d22946553090dcaacb6272db160a173
janN <m1lk9_w4y@protonmail.com>
2024-03-22 18:02:23 +0100

    Your commit message here

--- apps/analyses/apps.py
+++ apps/analyses/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
+
 class AnalysisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.analyses'
-
-    def ready(self):
-        # Safe place to import models and tasks
-        from . import signals  # Example: if you have signals.py
-        from .tasks import my_celery_task  # Example: importing a Celery task
--- apps/analyses/tasks.py
+++ apps/analyses/tasks.py
@@ -1,12 +1,7 @@
+# apps/analyses/tasks.py
 from celery import shared_task
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
-from .views import sentiment_analysis  # This imports your analysis function
-
 
 @shared_task
 def perform_analysis(message):
-    print(message)
-    # Call the sentiment_analysis function from views.py
-    result = sentiment_analysis(message)
-    print(result)
+    from .views import sentiment_analysis
+    # Use sentiment_analysis here
